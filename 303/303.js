02e0b7e217a072a7341ba52380913299e6f39b96
David Barron <dnbarron@gmail.com>
2023-02-19 21:53:22 +0000

    Correct sizing

--- 303/303.js
+++ 303/303.js
@@ -1,6 +1,7 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  gutter = 200;
+  createCanvas(window.innerWidth, window.innerHeight);
+  
+  gutter = 400;
   sqsize = width/2 - gutter;
   translate(sqsize/2 + gutter/2, height/2);
   rectMode(CENTER);
@@ -12,13 +13,13 @@ function setup() {
   
   // second part
   
-  translate(sqsize + gutter, 0);
+  translate(sqsize + gutter*.5, 0);
   rect(0, 0, sqsize - 200, sqsize);
   // trapezium
   quad(-sqsize/2, -sqsize/2, sqsize/2, -sqsize/2, 
        sqsize/2 + 100, sqsize/2, -sqsize/2 - 100, sqsize/2);
   // paralellogram
-  quad(-sqsize/2, -sqsize/2, sqsize/2 + 100, -sqsize/2,
+  quad(-sqsize/2, -sqsize/2, sqsize/2+100 , -sqsize/2,
        sqsize/2, sqsize/2, -sqsize/2 - 100, sqsize/2);
 }
 
