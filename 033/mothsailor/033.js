7d008476431efcd675b7bfb2d77a571ac4b74cee
David Barron <dnbarron@gmail.com>
2023-03-02 19:05:57 +0000

    Tidy #33 code

--- 033/mothsailor/033.js
+++ 033/mothsailor/033.js
@@ -1,48 +1,48 @@
-    function setup() {
-      createCanvas(windowHeight, windowHeight);
-      gridSize = height/10;
-      for (let  x = 0; x < width; x += gridSize) {
-        for (let y = 0; y < height; y += gridSize){
-          stroke(200);
-          strokeWeight(1);
-          square(x, y, gridSize);
-          strokeWeight(3);
-          cell = 0;
-          while (cell == 0) {
-            r = random();
-            if (r < 0.3) {
-              cell = 1;
-              stroke(0);
-              line(x + gridSize/2, y, x + gridSize/2, y + gridSize);
-            }
-            r = random();
-            if (r < 0.3) {
+function setup() {
+  createCanvas(windowHeight, windowHeight);
+  var gridSize = height/10;
+  for (var  x = 0; x < width; x += gridSize) {
+    for (var y = 0; y < height; y += gridSize){
+      stroke(200);
+      strokeWeight(1);
+      square(x, y, gridSize);
+      strokeWeight(3);
+      var cell = 0;
+      while (cell == 0) {
+        var r = random();
+        if (r < 0.3) {
+          cell = 1;
+          stroke(0);
+          line(x + gridSize/2, y, x + gridSize/2, y + gridSize);
+        }
+        r = random();
+        if (r < 0.3) {
   // diagonal lines        
-              rr = random();
-              cell = 1;
-              if (rr < 0.5) {
+          var rr = random();
+          cell = 1;
+          if (rr < 0.5) {
     // blue lines            
-                stroke(0, 0, 255);
-                line(x, y, x + gridSize, y + gridSize);
-              } 
-              else { 
+            stroke(0, 0, 255);
+            line(x, y, x + gridSize, y + gridSize);
+          } 
+          else { 
     // red lines            
-                  stroke(255, 0, 0);
-                  line(x + gridSize, y, x , y + gridSize);
-              } 
-            }  
-            r = random();
+              stroke(255, 0, 0);
+              line(x + gridSize, y, x , y + gridSize);
+          } 
+        }  
+        r = random();
   // yellow line          
-            if (r < 0.3){
-               cell = 1;
-               stroke(255, 255, 0);
-              line(x, y + gridSize/2, x + gridSize, y + gridSize/2);
-            }
-          }
+        if (r < 0.3){
+            cell = 1;
+            stroke(255, 255, 0);
+            line(x, y + gridSize/2, x + gridSize, y + gridSize/2);
         }
       }
     }
+  }
+}
 
-    function draw() {
+function draw() {
   
-    }
+}
